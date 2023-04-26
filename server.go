42ba981589fea758b9bdd7ae4d3c2e4ec7de2236
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2023-04-26 23:25:49 +0200

    fix: allow any origin for ws

--- server.go
+++ server.go
@@ -6,13 +6,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"gqlgen-subscriptions/graph/generated"
 	m "gqlgen-subscriptions/graph/model"
 	"gqlgen-subscriptions/graph/resolvers"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gorilla/websocket"
 )
 
 const defaultPort = "8080"
@@ -25,7 +30,27 @@ func main() {
 
 	eventChannel := make(chan *m.Event)
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolvers.Resolver{EventChannel: eventChannel}}))
+	srv := handler.New(generated.NewExecutableSchema(generated.Config{Resolvers: &resolvers.Resolver{EventChannel: eventChannel}}))
+
+	srv.AddTransport(transport.Websocket{
+		KeepAlivePingInterval: 10 * time.Second,
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+	})
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.MultipartForm{})
+
+	srv.SetQueryCache(lru.New(1000))
+
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New(100),
+	})
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
 	http.Handle("/graphql", srv)
